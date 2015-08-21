21da8473b372101dcd2a35b53e546d8c59d47872
Brrrutal <brutal@Serhiys-MBP.home>
2015-08-20 23:08:57 -0400

    More boilerplate code.

--- clouds.js
+++ clouds.js
@@ -5,3 +5,33 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+directionalLight.position.set( 0, 1, 0 );
+scene.add( directionalLight );
+ 
+var geometry = new THREE.TorusGeometry( 10, 4, 16, 100 );
+var material = new THREE.MeshPhongMaterial( 
+    { 
+        color: 0xccffee, 
+        specular: 0x009900, 
+        shininess: 30, 
+        shading: THREE.SmoothShading 
+    });
+
+var cube = new THREE.Mesh( geometry, material ); 
+
+scene.add( cube ); 
+camera.position.z = 50;
+
+function render() 
+{ 
+    requestAnimationFrame( render ); 
+
+    cube.rotation.x += 0.02; 
+    cube.rotation.y += 0.02;
+
+    renderer.render( scene, camera ); 
+} 
+
+render();
+
