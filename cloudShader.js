1a5ccf1d26ada1bb22606f795da08e1137ad3255
Serhiy Likhanov <slikhan@gmail.com>
2015-10-14 00:22:07 -0400

    Custom shader for cloud sprites.

---
+++ cloudShader.js
@@ -0,0 +1,24 @@
+CloudShader = 
+{
+    vertexShader: 
+    [
+        "varying vec2 vUv;",
+        "void main()",
+        "{",
+            "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+            "vUv = uv;",
+        "}"
+    ].join("\n"),
+
+    fragmentShader: 
+    [
+        "varying vec2 vUv;",
+        "void main()",
+        "{",
+            "vec2 uv = vUv - vec2(0.5, 0.5);",
+            "float dist = 1.0 - step(0.45, sqrt(dot(uv, uv)));",
+            "gl_FragColor = vec4(dist, dist, dist, dist);",
+        "}"
+    ].join("\n")
+};
+
--- clouds.js
+++ clouds.js
@@ -1,28 +1,28 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+// Setting up renderer and hooking it up to the web page view.
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( 0x00aaff );
 document.body.appendChild( renderer.domElement );
 
+// Setting up main scene, camera and light.
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 50;
+
 var light = new THREE.PointLight( 0xffffff, 1, 100 );
 light.position.set( 20, 20, 20 );
 scene.add( light );
  
+// Setting up scene geometry.
 var geometry = new THREE.PlaneGeometry( 20, 20 );
 var geometry2 = new THREE.PlaneGeometry( 20, 20 );
-var material = new THREE.MeshPhongMaterial( 
+var shader = CloudShader;
+var material = new THREE.ShaderMaterial( 
     { 
         transparent: true,
-        //color: 0xccffee, 
-        //specular: 0x009933, 
-        emissive: 0x888888,
-        shininess: 10, 
-        shading: THREE.SmoothShading,
-        map: THREE.ImageUtils.loadTexture("maps/00_000.png"),
-        normalMap: THREE.ImageUtils.loadTexture(
-                "maps/normalmap_beethovennormalmap.jpg" ),
+        vertexShader: shader.vertexShader,
+        fragmentShader: shader.fragmentShader
     });
 
 var cube = new THREE.Mesh( geometry, material ); 
@@ -30,7 +30,6 @@ var cube2 = new THREE.Mesh( geometry2, material );
 
 scene.add( cube ); 
 scene.add( cube2 );
-camera.position.z = 50;
 
 function render() 
 { 
